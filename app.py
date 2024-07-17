5930dbecefacefa7c481d95447b092caba776009
Kakarlapudi Rajeswari <127588101+rajikakarlapudi@users.noreply.github.com>
2024-07-17 10:20:19 +0530

    Update for removing the share attribute and added port by your god app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
+import os
 import gradio as gr
 import edge_tts
 import asyncio
 import random
-import os
 import time
 
 async def tts(text, voice, speed, output_file):
-    # Ensure the rate is in the correct format (+10%, -10%, etc.)
     if speed > 0:
         rate_str = f"+{speed}%"
     elif speed < 0:
         rate_str = f"{speed}%"
     else:
-        rate_str = "0%"  # Default to 0% if no speed change
+        rate_str = "0%"
 
-    print(f"Using rate: {rate_str}")  # Log the rate being used
     communicate = edge_tts.Communicate(text, voice, rate=rate_str)
     await communicate.save(output_file)
 
@@ -23,21 +21,16 @@ def infer_tts_audio(tts_voice, tts_text, tts_speed):
     os.makedirs(folder_tts, exist_ok=True)
     out_path = os.path.join(folder_tts, f"tts_{random.randint(10000, 99999)}.mp3")
 
-    # Convert tts_speed from string to integer
-    speed_value = int(tts_speed.rstrip('%'))  # Extract the numerical value from the percentage string
-
+    speed_value = int(tts_speed.rstrip('%'))
     start_time = time.time()
     asyncio.run(tts(tts_text, tts_voice, speed_value, out_path))
     end_time = time.time()
-    print(f"Total processing time: {end_time - start_time} seconds")
 
     return out_path
 
-# List available voices
 voices = asyncio.run(edge_tts.list_voices())
 voice_options = sorted([v['ShortName'] for v in voices])
 
-# Define speed options
 speed_options = ["-100%", "-90%", "-80%", "-70%", "-60%", "-50%", "-40%", "-30%", "-20%", "-10%", "+10%", "+20%", "+30%", "+40%", "+50%", "+60%", "+70%", "+80%", "+90%", "+100%"]
 
 def get_gui():
@@ -65,5 +58,6 @@ def get_gui():
 
 if __name__ == "__main__":
     app = get_gui()
+    port = int(os.environ.get("PORT", 7860))
     app.queue()
-    app.launch(share=True)
+    app.launch(server_name="0.0.0.0", server_port=port)  # Removed share=True
